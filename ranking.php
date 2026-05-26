1b74f2ca3dfba8320a8918175e35c74f3924c1a1
Gabriel <60550099+gabrielgutemberg@users.noreply.github.com>
2026-05-26 16:15:17 -0300

    tabela inicial feita

--- ranking.php
+++ ranking.php
@@ -2,11 +2,42 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ranking</title>
+    <title>Ranking de Álbuns - Home</title>
+    <link rel="stylesheet" href="assets/styles.css?v=1">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo+Black&family=Google+Sans:ital,opsz,wght@0,17..18,400..700;1,17..18,400..700&display=swap" rel="stylesheet">
 </head>
 <body>
-    <h1>RANKINGGGGGG</h1>
-    <p>testeee</p> 
+    <header>
+        <div class="notaMus">Nota Musical</div>
+        <nav>
+            <ul>
+            <li><a href = "index.php"><svg class="icone" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><path d="M39.5,43h-9c-1.381,0-2.5-1.119-2.5-2.5v-9c0-1.105-0.895-2-2-2h-4c-1.105,0-2,0.895-2,2v9c0,1.381-1.119,2.5-2.5,2.5h-9	C7.119,43,6,41.881,6,40.5V21.413c0-2.299,1.054-4.471,2.859-5.893L23.071,4.321c0.545-0.428,1.313-0.428,1.857,0L39.142,15.52	C40.947,16.942,42,19.113,42,21.411V40.5C42,41.881,40.881,43,39.5,43z"/></svg>Inicio</a></li>
+            <span>|</span>
+            <li><a href = "ranking.php"><svg class = "icone" xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 1024 1024">
+  <path d="M834.1 469.2A347.49 347.49 0 0 0 751.2 354l-29.1-26.7a8.09 8.09 0 0 0-13 3.3l-13 37.3c-8.1 23.4-23 47.3-44.1 70.8-1.4 1.5-3 1.9-4.1 2-1.1.1-2.8-.1-4.3-1.5-1.4-1.2-2.1-3-2-4.8 3.7-60.2-14.3-128.1-53.7-202C555.3 171 510 123.1 453.4 89.7l-41.3-24.3c-5.4-3.2-12.3 1-12 7.3l2.2 48c1.5 32.8-2.3 61.8-11.3 85.9-11 29.5-26.8 56.9-47 81.5a295.64 295.64 0 0 1-47.5 46.1 352.6 352.6 0 0 0-100.3 121.5A347.75 347.75 0 0 0 160 610c0 47.2 9.3 92.9 27.7 136a349.4 349.4 0 0 0 75.5 110.9c32.4 32 70 57.2 111.9 74.7C418.5 949.8 464.5 959 512 959s93.5-9.2 136.9-27.3A348.6 348.6 0 0 0 760.8 857c32.4-32 57.8-69.4 75.5-110.9a344.2 344.2 0 0 0 27.7-136c0-48.8-10-96.2-29.9-140.9z"/>
+</svg>Ranking</a></li>
+        </ul>
+        </nav>
+    </header>
+    <div class = "principal">
+        <div class = "topo">
+            <h1>Ranking</strong></h1>
+            <button class = "botaoAdd" >Adicionar album ao rank</button>
+        </div>
+        <table>
+            <tr>
+                <th>Capa</th>
+                <th>Nota</th>
+                <th>Titulo</th>
+                <th>Artista</th>
+                <th>Genero</th>
+                <th>Ano</th>
+                <th>Faixas</th>
+                <th>Ações</th>
+            </tr>
+        </table>
+    </div>
 </body>
 </html>
